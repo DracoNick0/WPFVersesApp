f4ca2492d6c50d82096ffdbf20571631b833142a
DracoNick0 <52973610+DracoNick0@users.noreply.github.com>
2024-07-16 11:07:57 -0700

    Add project files.

---
+++ WpfApp1/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Core;
+
+namespace WpfApp1.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        
+        public RelayCommand SavedVersesViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public SavedVersesViewModel SavedVersesVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            { 
+                _currentView = value;
+                OnPropteryChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            SavedVersesVM = new SavedVersesViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            SavedVersesViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SavedVersesVM;
+            });
+        }
+    }
+}
