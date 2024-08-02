80347ca283a3405171fd157dba32489fe9426899
DracoNick0 <52973610+DracoNick0@users.noreply.github.com>
2024-08-02 09:45:51 -0700

    Added taptoreveal view and viewmodel, needs more work.

---
+++ WpfApp1/MVVM/ViewModel/TapToRevealViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Core;
+
+namespace WpfApp1.MVVM.ViewModel
+{
+    class TapToRevealViewModel
+    {
+        private List<string> fullPassage;
+
+        private string revealedPassage;
+        public string RevealedPassage
+        {
+            get { return revealedPassage; }
+        }
+
+
+        public TapToRevealViewModel(Verse verse)
+        {
+            char[] delimiters = new char[] { ',', '.', ';', ':' };
+            int prevDelimiter = 0;
+
+            for (int i = 0; i < verse.passage.Length; i++)
+            {
+                foreach (char delimiter in delimiters)
+                {
+                    if (verse.passage[i] == delimiter)
+                    {
+                        fullPassage.Add(verse.passage.Substring(prevDelimiter, i - prevDelimiter + 1));
+                    }
+                }
+            }
+        }
+
+    }
+}
