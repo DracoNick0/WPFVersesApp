604d7cfc5c908ca57c61bbc6e33becaa6fa94f1d
DracoNick0 <52973610+DracoNick0@users.noreply.github.com>
2024-08-29 10:07:33 -0700

    PassageMemory works as intended. Just want to implement textbox focus on entry.

---
+++ WpfApp1/MVVM/View/PassageMemoryView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WpfApp1.MVVM.ViewModel;
+
+namespace WpfApp1.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for TapToRevealView.xaml
+    /// </summary>
+    public partial class PassageMemoryView : UserControl
+    {
+        PassageMemoryViewModel PMVM { get; set; }
+
+        public PassageMemoryView()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            // RevealedPassageTextBox.Focus();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (DataContext is PassageMemoryViewModel passageMemoryViewModel)
+            {
+                var textBox = sender as TextBox;
+                if (textBox != null)
+                {
+                    passageMemoryViewModel.TextBoxChangedCommand.Execute(textBox.Text);
+
+                    // Move user cursor to end of text.
+                    textBox.CaretIndex = textBox.Text.Length;
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp1/MVVM/ViewModel/PassageMemoryViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using WpfApp1.Core;
+
+namespace WpfApp1.MVVM.ViewModel
+{
+    class PassageMemoryViewModel : ObservableObject
+    {
+        private List<string> fullPassage = new List<string>();
+        private int index = 0;
+
+        private string revealedPassage;
+        private string textBoxText;
+
+        public string TextBoxText
+        {
+            get { return textBoxText; }
+            set
+            {
+                OnTextChanged(value);
+            }
+        }
+
+        public RelayCommand TextBoxChangedCommand { get; set; }
+
+        public PassageMemoryViewModel(Verse verse)
+        {
+            char[] delimiters = new char[] { ' ', '\0' };
+            int prevDelimiter = -1;
+            string partOfPassage;
+            int i = 0;
+
+            for (; i < verse.passage.Length; i++)
+            {
+                foreach (char delimiter in delimiters)
+                {
+                    if (verse.passage[i] == delimiter)
+                    {
+                        partOfPassage = verse.passage.Substring(prevDelimiter + 1, i - prevDelimiter);
+                        this.fullPassage.Add(partOfPassage);
+                        prevDelimiter = i;
+                    }
+                }
+            }
+
+            partOfPassage = verse.passage.Substring(prevDelimiter + 1, i - prevDelimiter - 1);
+            this.fullPassage.Add(partOfPassage);
+
+            TextBoxChangedCommand = new RelayCommand(OnTextChanged);
+
+            revealedPassage = string.Empty;
+        }
+
+        private void OnTextChanged(object textBoxText)
+        {
+            string textBoxString = textBoxText as string;
+            // If the full passage hasn't been revealed yet
+            if (textBoxString != revealedPassage)
+            {
+                if (index != fullPassage.Count)
+                {
+                    if (textBoxString[textBoxString.Length - 1] == fullPassage[index].ToLower()[0])
+                    {
+                        RevealText();
+                    }
+                    else
+                    {
+                        SetTextBoxToRevealedText();
+                    }
+                }
+                else
+                {
+                    SetTextBoxToRevealedText();
+                }
+            }
+        }
+
+        private void RevealText()
+        {
+            revealedPassage += fullPassage[index];
+            index++;
+
+            SetTextBoxToRevealedText();
+        }
+
+        // Sets textBox text to revealedText, and notifies the textBox of change.
+        private void SetTextBoxToRevealedText()
+        {
+            textBoxText = revealedPassage;
+            OnPropertyChanged(nameof(TextBoxText));
+        }
+    }
+}
